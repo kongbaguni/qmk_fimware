bf1f43748276f46ef780617579a339eb72b99de7
kongbaguni <kongbaguni@gmail.com>
2020-07-30 11:06:07 +0900

    자동시프트 기능 추가. commend_enable 기능 비활성화 (용량 확보)

--- keyboards/crkbd/keymaps/kongbaguni/rules.mk
+++ keyboards/crkbd/keymaps/kongbaguni/rules.mk
@@ -7,7 +7,7 @@ BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = yes        # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
-COMMAND_ENABLE = yes         # Commands for debug and configuration
+COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
@@ -17,7 +17,9 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 TAP_DANCE_ENABLE = no
-OLED_DRIVER_ENABLE = yes
+OLED_DRIVER_ENABLE = yes 
+AUTO_SHIFT_ENABLE = yes
+
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
